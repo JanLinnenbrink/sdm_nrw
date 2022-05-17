af2fd0f416fec322490db84bab26bbd24722a0de
JanLinnenbrink <jan.linnenbrink@web.de>
2022-05-17 20:57:56 +0200

    code updated

--- predictors_prep.R
+++ predictors_prep.R
@@ -2,11 +2,6 @@ require(sf)
 require(fasterize)
 library(terra)
 
-# change path to the folder with your shapefiles
-path <- "C:/0_Msc_Loek/M7_Fernerkundung/shapes"
-
-# change fields according to the relevant fields in your shape files
-fields <- list("BAUMART", "ZIEL", "HECKENART", "NUTZUNG","OBJECTID")
 
 vc2ras <- function(path, resolution, fields) {
   env_vector_ls <- paste0(path, "/", list.files(path, pattern = "*.shp"))
@@ -22,10 +17,22 @@ vc2ras <- function(path, resolution, fields) {
   message("converting to raster")
   
   ras_ls <- list()
-  for(i in seq_along(env_vector)) {ras_ls[[i]]=terra::rasterize(env_spatvec[[i]],raster_template,field=fields[[i]]);return(x)}
+  for(i in seq_along(env_vector)) {ras_ls[[i]]=terra::rasterize(env_spatvec[[i]],raster_template,field=fields[[i]])}
   
-  env_raster <- stack(ras_ls)
+  message("stacking")
+  
+  return(stack(lapply(ras_ls, raster)))
 }
 
-env_data <- vc2ras(path=path, resolution = 10, fields = fields)
+
+# change path to the folder with your shapefiles
+path <- "C:/0_Msc_Loek/M7_Fernerkundung/shapes"
+
+# change fields according to the relevant fields in your shape files
+fields <- list("BAUMART", "ZIEL", "HECKENART", "NUTZUNG","OBJECTID")
+
+# resolution in meters
+resolution <- 10
+
+env_data <- vc2ras(path=path, resolution = resolution, fields = fields)
 
