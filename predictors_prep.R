54c03b7c04c9261763ea9b29432f232358ce0ecd
JanLinnenbrink <jan.linnenbrink@web.de>
2022-05-17 20:41:16 +0200

    ccd

--- predictors_prep.R
+++ predictors_prep.R
@@ -1,10 +1,29 @@
-require(stars)
 require(sf)
+require(fasterize)
+library(terra)
 
-vc2ras <- function(path) {
-  env_vector_ls <- list.files(path, pattern = "*.shp")
+path <- "C:/0_Msc_Loek/M7_Fernerkundung/shapes"
+
+fields <- list("BAUMART", "ZIEL", "HECKENART", "NUTZUNG","OBJECTID")
+
+vc2ras <- function(path, resolution, fields) {
+  env_vector_ls <- paste0(path, "/", list.files(path, pattern = "*.shp"))
   env_vector <- lapply(env_vector_ls, st_read)
-  env_raster <- stack(lapply(env_vector, st_rasterize))
+  
+  raster_template <- rast(ext(env_vector[[1]]), resolution = 10, 
+                          crs = st_crs(env_vector[[1]])$wkt)
+  
+  message("converting to spatvec")
+  
+  env_spatvec <- lapply(env_vector, terra::vect)
+  
+  message("converting to raster")
+  
+  ras_ls <- list()
+  for(i in seq_along(env_vector)) {ras_ls[[i]]=terra::rasterize(env_spatvec[[i]],raster_template,field=fields[[i]]);return(x)}
+  
+  env_raster <- stack(ras_ls)
 }
 
+env_data <- vc2ras(path=path, resolution = 10, fields = fields)
 
