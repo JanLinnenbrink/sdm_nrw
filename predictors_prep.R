23a7ae2a073c040b1decda575d2489ce82bc5038
JanLinnenbrink <jan.linnenbrink@web.de>
2022-05-17 19:52:30 +0200

    Ab

--- predictors_prep.R
+++ predictors_prep.R
@@ -1,3 +1,10 @@
-require(raster)
+require(stars)
+require(sf)
+
+vc2ras <- function(path) {
+  env_vector_ls <- list.files(path, pattern = "*.shp")
+  env_vector <- lapply(env_vector_ls, st_read)
+  env_raster <- stack(lapply(env_vector, st_rasterize))
+}
 
 
