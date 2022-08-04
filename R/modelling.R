e018d822857a679f55980a8799ff255dcad5e445
JanLinnenbrink <jan.linnenbrink@web.de>
2022-08-04 20:34:16 +0200

    distances calculated

--- R/modelling.R
+++ R/modelling.R
@@ -1,18 +1,9 @@
 library(caret)
 library(CAST)
-library(NNDM)
 library(terra)
 library(sf)
 library(dplyr)
-library(purrr)
-
-library("NNDM")
-library("caret")
-library("sp")
-library("sf")
-library("knitr")
-library("gstat")
-library("gridExtra")
+library(ggplot2)
 
 
 ### load data
@@ -21,14 +12,14 @@ library("gridExtra")
 #occs_prep(occurences_sf = occs, env_data = env_data, 
 #          path_out = "C:/0_Msc_Loek/M7_Fernerkundung/data_sdm_nrw/data/occurence_data")
 
-train_dat <- readRDS("e:/sdm_nrw/train_dat")
+train_dat <- readRDS("mnt/e:/sdm_nrw/train_dat")
 
 preds_n <- names(train_dat[!names(train_dat) %in% c("type", "ID")])
 
 ### create spatial cv folds
 set.seed(1234)
 
-occs <- st_read("C:/0_Msc_Loek/M7_Fernerkundung/sdm_nrw/R/occurences.gpkg") %>%
+occs <- st_read("mnt/c/0_Msc_Loek/M7_Fernerkundung/sdm_nrw/R/occurences.gpkg") %>%
   mutate(FID = 1:nrow(.)) %>% 
   .[,"FID"]
 bgs <- st_read("E:/sdm_nrw/bg_vect.shp")
@@ -55,7 +46,7 @@ st_geometry(background_g) <- "geom"
 
 points <- rbind(occs_g,background_g)
 
-st_write(points, "E:/sdm_nrw/training_points.shp", append=FALSE)
+st_write(points, "~/R_output/training_points.shp", append=FALSE)
 
 #points <- st_read("E:/sdm_nrw/training_points.shp")
 
@@ -65,14 +56,14 @@ pnts_plot <- ggplot(points, aes(alpha = as.factor(pres), colour=as.factor(cl))) 
                        range = c(0.3,1)) +
   scale_colour_discrete(name= "Cluster")
 
-ggsave("C:/0_Msc_Loek/M7_Fernerkundung/sdm_nrw/Feldgrille_plots/points.pdf", pnts_plot)
+ggsave("~/R_output/points.pdf", pnts_plot)
 
 
 trainDat <- data.frame(cl=points$cl, pres = points$pres,
                        terra::extract(predictors, vect(points), df=TRUE))%>% 
   .[complete.cases(.),]
 
-saveRDS(trainDat, "E:/sdm_nrw/trainDat")
+saveRDS(trainDat, "~/R_output/sdm_nrw/trainDat")
 
 
 ### ffs
@@ -93,10 +84,12 @@ model_ffs <- ffs(predictors = trainDat[,names(trainDat) %in% names(predictors)],
 
 stopCluster(cl)
 
-ggplot(varImp(model_ffs)) +
+vimp <- ggplot(varImp(model_ffs)) +
   geom_bar(width=1, stat="identity")
 
-saveRDS(model_ffs,file="E:/sdm_nrw/ffs_model") 
+ggsave("~/R_output/vimp.pdf", vimp)
+
+saveRDS(model_ffs,file="~/R_output/sdm_nrw/ffs_model") 
 final_model <- model_ffs$finalModel
 
 plot_ffs(model_ffs)
@@ -104,20 +97,4 @@ plot_ffs(model_ffs,plotType="selected")
 
 sel_vars <- model_ffs$selectedvars
 
-writeRaster(predictors, "E:/sdm_nrw/predictors_aktuell.tif")
-
-model_ffs <- readRDS("E:/sdm_nrw/ffs_model")
-
-
-ndvi <- predictors[[1]]
-
-rasterVis::gplot(ndvi) +
-  geom_tile(aes(fill=ndvi$ndvi))
-
-myTheme <- rasterVis::magmaTheme()
-myTheme$panel.background$col = 'white' 
-
-plots <- rasterVis::levelplot(predictors,par.settings = myTheme,
-                              maxpixels=ncell(ndvi)*0.01)
-
-spplot(ndvi, maxpixels=ncell(ndvi)*0.2)
+print(sel_vars)
